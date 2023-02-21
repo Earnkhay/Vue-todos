b366f654f6a8a9a58d2e0bee10af9d6d4c34ffb0
Julia <earnkay2@gmail.com>
2023-02-21 16:44:29 +0100

    first commit

---
+++ src/components/TodoButton.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <button>
+            <slot>Button</slot>
+        </button>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+ button {
+    padding: 8px 16px;
+    border: none;
+  }
+</style>
